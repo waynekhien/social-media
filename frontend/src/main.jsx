b38816d6ebc5fab1ae1c546f0d64bf7278add8c3
waynekhien <khien23092003@gmail.com>
2025-06-25 22:58:02 +0700

    feat : Auth handled and routes protected in client

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; 
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+        },
+    },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <Toaster />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
